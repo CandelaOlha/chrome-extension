bf4140d27d3918db2d886db820631d08ffc7e95b
“c.b.olha” <c.b.olha@viseven.com>
2023-03-07 16:53:37 -0300

    added code to display list of leads

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const inputEl = document.querySelector("#inputEl");
 const saveButton = document.querySelector("#saveButton");
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const leads = document.querySelector("#leads");
+let itemsList = "";
 
 const saveLead = () => {
-  console.log("Lead saved!");
+  myLeads.push(inputEl.value);
+  //   console.log(myLeads);
 };
 
 saveButton.addEventListener("click", saveLead);
+
+for (let i = 0; i < myLeads.length; i++) {
+  itemsList += `<li>${myLeads[i]}</li>`;
+}
+
+leads.innerHTML = itemsList;
